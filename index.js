2fc6c99f8565f2759a7b95959e01beab0c504a0c
Cameron Ornelas <cvornelas4@gmail.com>
2019-12-29 00:16:15 -0600

    Revision Complete

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ const myQuestions = [
   //increase user score by 1
   function correctAnswer() {
     $('.response').html(
-      `<h2>Your answer is correct!</h2>
+      `<h4>Your answer is correct!</h4>
       <img src="img/correct-img.jpg" alt="smart dog" class="images" width="200px">
         <p class="sizeMe">Correct!</p>
         <button type="button" class="nextButton button">Next</button>`
@@ -193,7 +193,7 @@ const myQuestions = [
   //populate feedback (incorrect)
   function wrongAnswer() {
     $('.response').html(
-      `<h2>Sorry, that answer is wrong.</h2>
+      `<h4>Sorry, that answer is wrong.</h4>
       <img src="img/wrong-img.jpg" alt="dissapointed dog" class="images" width="200px">
       <p class="sizeMe">It's actually:</p>
       <p class="sizeMe">${myQuestions[questionNumber].correctAnswer}</p>
